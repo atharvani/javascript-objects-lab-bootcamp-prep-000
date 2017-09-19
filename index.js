7b86fee1058e365158ebd0c78a7965a5164d7d3a
Shital Sanap <shital.sanap@gmail.com>
2017-09-19 04:32:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var recipes = new Object();
 function updateObjectWithKeyAndValue(obj, key, value)
 {
   var newObj = new Object();
-  newObj = Object.assign({},obj,{key: value});
+  newObj = Object.assign({},obj,{[key]: value});
   return newObj;
 }
